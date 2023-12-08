387f50cb626e43e52c2397c84c3ff88f35b02dc4
22DP1MHeid <131352976+22DP1MHeid@users.noreply.github.com>
2023-12-08 12:06:28 +0000

    Stundas darbs (neizdevas mainigos pievienot html)

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DefaultController {
-    
-   @GetMapping(value = "/")
+
+    @GetMapping(value = "/")
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
+    }
+    
+    @GetMapping(value = "/about")
+    String aboutMeAction(Model model) {
+        String name = "Markuss";
+        String surname = "Heidemanis";
+        int age = 18;
+        String group = "DP2-1";
+        // model.addAttribute();
+
+        return "about";
     }
 }
