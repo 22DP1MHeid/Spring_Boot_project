1497d1d84cecce65a8b79b16a569156a6e4cb2de
22DP1MHeid <131352976+22DP1MHeid@users.noreply.github.com>
2024-01-23 18:22:34 +0000

    Pievienoju CardPayments pirmo soli

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -7,5 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+
+        PaymentCard petesCard = new PaymentCard(10);
+
+        System.out.println("money " + petesCard.balance());
+        boolean wasSuccessful = petesCard.takeMoney(8);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
+
+        wasSuccessful = petesCard.takeMoney(4);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
     }
 }
