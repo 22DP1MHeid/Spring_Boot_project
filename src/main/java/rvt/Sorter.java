bd3ab69f176d306e45af6fde875835292919b84d
22DP1MHeid <131352976+22DP1MHeid@users.noreply.github.com>
2024-01-30 10:43:05 +0000

    pievienoju index of smallest from metodi un testu

--- src/main/java/rvt/Sorter.java
+++ src/main/java/rvt/Sorter.java
@@ -15,12 +15,25 @@ public class Sorter {
 
     public static int indexOfSmallest(int[] array){
         int smallest = array[0];
-        int smallestIndex = array[0];
+        int smallestIndex = 0;
 
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] < smallest) {
                 smallest = array[i];
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int smallest = table[startIndex];
+        int smallestIndex = startIndex;
+
+        for (int i = startIndex; i < table.length; i++) {
+            if (table[i] < smallest) {
+                smallest = table[i];
                 smallestIndex = i;
             }
         }
--- src/test/java/lv/rvt/SorterTest.java
+++ src/test/java/lv/rvt/SorterTest.java
@@ -17,10 +17,18 @@ public class SorterTest {
     }
 
     @Test
-    public void indexOfSmallest() {
+    public void testIndexOfSmallest() {
         int[] intArray = {3, 5, 10, 500, 17, 2};
 
         assertEquals(5, Sorter.indexOfSmallest(intArray));
+    }
+
+    @Test
+    public void testIndexOfSmallestFrom() {
+        int[] intArray = {3, 5, 1, 6, 7, 10, 24, 5, 1};
+        int startIndex = 3;
+        
+        assertEquals(8, Sorter.indexOfSmallestFrom(intArray, startIndex));
     }
 }
 
